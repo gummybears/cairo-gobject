fb3ca8cc16b630ac7b0a14ba0f15d353df00096c
icubic <wneimeijer@xs4all.nl>
2021-07-06 09:54:29 +0200

    Updated src/lib_cairo.cr to work with Crystal 1.0.0

--- spec/cairo-gobject_spec.cr
+++ spec/cairo-gobject_spec.cr
@@ -1,9 +1,5 @@
 require "./spec_helper"
 
-describe Cairo::Gobject do
+describe "Cairo::Gobject" do
   # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
-  end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/cairo-gobject"
+#require "../src/cairo-gobject"
